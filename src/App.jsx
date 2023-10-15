711e2d1797e92d3db5fcfd4bd9ba524ba96f6194
jose medina <chesibb@gmail.com>
2023-10-15 19:56:31 -0300

    footer creado por chesi

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,34 @@ import Formulario from "./components/Formulario";
 import { BaseColaboradores } from "./data/object";
 import { useState } from "react";
 import Buscador from "./components/Buscador";
+import Footer from "./components/Footer";
 
 function App() {
-  const [collaborators, setCollaborators] = useState(BaseColaboradores);
-  return (
-    <main>
-      <div>
-        <h1>Lista de Colaboradores</h1>
-        <Buscador setCollaborators={setCollaborators} BaseColaboradores={BaseColaboradores}></Buscador>
-        <Listado colaboradores={collaborators} BaseColaboradores={BaseColaboradores} setCollaborators={setCollaborators}></Listado>
-      </div>
-      <Formulario colaboradores={collaborators} setCollaborators={setCollaborators} BaseColaboradores={BaseColaboradores}></Formulario>
-    </main>
-  );
+	const [collaborators, setCollaborators] = useState(BaseColaboradores);
+	return (
+		<div>
+			<main>
+				<div>
+					<h1>Lista de Colaboradores</h1>
+					<Buscador
+						setCollaborators={setCollaborators}
+						BaseColaboradores={BaseColaboradores}
+					></Buscador>
+					<Listado
+						colaboradores={collaborators}
+						BaseColaboradores={BaseColaboradores}
+						setCollaborators={setCollaborators}
+					></Listado>
+				</div>
+				<Formulario
+					colaboradores={collaborators}
+					setCollaborators={setCollaborators}
+					BaseColaboradores={BaseColaboradores}
+				></Formulario>
+			</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<footer>
+			<p>este footer lo hizo el chesi </p>
+		</footer>
+	);
+};
+
+export default Footer;
